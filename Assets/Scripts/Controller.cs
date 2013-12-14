3453274af6827bc81c6ed11581eb9ef849384a3f
some_lun <info@lunatic.su>
2013-12-14 12:53:15 +0700

    forget to commit so here is a lot of changes

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+	public enum ControllerState {
+		Rest = 0,
+		Left,
+		Right,
+	};
+
+	ControllerState state = ControllerState.Rest;
+	ControllerState lastState = 0;
+
+	public float walkingSpeed = 15.0f;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	void FixedUpdate() {
+        int horizontal = (int) Input.GetAxis("Horizontal");
+
+        lastState = 0;
+
+        switch (horizontal) {
+        	case 1:
+        		state = ControllerState.Right;
+        		Debug.Log("right");
+        		break;
+
+        	case -1:
+        		state = ControllerState.Left;
+        		Debug.Log("left");
+        		break;
+
+        	default:
+        		break;
+        }
+
+        lastState = state;
+
+        Vector3 currentSpeed = this.rigidbody2D.velocity;
+        this.rigidbody2D.velocity = new Vector3(horizontal * walkingSpeed, currentSpeed.y, 0);
+	}
+}
