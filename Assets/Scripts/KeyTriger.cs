b64654d353ecddcdba82530d3d789121994ed014
some_lun <info@lunatic.su>
2013-12-15 12:13:06 +0700

    add animations

---
+++ Assets/Scripts/KeyTriger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyTriger : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		Destroy(this.gameObject);
+	}
+}
